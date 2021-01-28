e63c5540b893861459ea7282057d160a25d44ef2
madobyte <marcbambi@gmail.com>
2021-01-28 18:22:29 +0800

    Added mouse and keyboard compatibility. Added a decent GUI with transitions. Fixed minor bugs.

--- script.js
+++ script.js
@@ -3,46 +3,65 @@ function drumSet() {
     section.id = "drum-kit";
     document.body.prepend(section)
     for (element in data) {
-        const drumEl = document.createElement('div');
+        const drumEl = document.createElement('button');
         drumEl.classList.add(data[element].name);
         drumEl.classList.add("drum");
-        drumEl.innerText = data[element].name;
+        drumEl.addEventListener('click', playDrum)
+        drumEl.innerText = `${data[element].key} \n ${data[element].name}`
         const drumKit = document.getElementById('drum-kit');
         drumKit.appendChild(drumEl);
     }
 }
 
-/* Another way to play */
-/* function playDrum(e) {
-    const audio = document.createElement('audio')
-    const source = document.createElement('source')
-    source.src = data[e.code].sound;
-    source.type = "audio/wasaasasasasasv";
-    audio.appendChild(source);
-    audio.play();
-} */
-
 function playDrum(e) {
-    console.log(e.code)
+    /* Keydown event */
     if ( data.hasOwnProperty(e.code) ) {
         const audio = document.createElement('audio')
         audio.src = data[e.code].sound;
-        audio.load();
+        const targetButton = document.querySelector(`.${data[e.code].name}`)
+        targetButton.classList.add('playing');
+        if (!audio) return;
+        audio.currentTime = 0;
         audio.play();
+
+    } 
+    /* Mouse click event */
+    for(element in data) {
+        const dataName = Object.values(data[element])[1];
+        const dataSound = Object.values(data[element])[2];
+        const className = e.target.classList[0];
+        if (dataName === className) {
+            const audio = document.createElement('audio')
+            audio.src = dataSound;
+            const targetButton = document.querySelector(`.${e.target.classList[0]}`)
+            targetButton.classList.add('playing')
+            if (!audio) return;
+            audio.currentTime = 0;
+            audio.play();
+        }
     }
 }
 
-const data = {
-    'KeyA': {name: 'boom', sound: './assets/sounds/boom.wav'},
-    'KeyS': {name: 'clap', sound: './assets/sounds/clap.wav'},
-    'KeyD': {name: 'hihat', sound: './assets/sounds/hihat.wav'},
-    'KeyF': {name: 'kick', sound: './assets/sounds/kick.wav'},
-    'KeyG': {name: 'openhat', sound: './assets/sounds/openhat.wav'},
-    'KeyH': {name: 'ride', sound: './assets/sounds/ride.wav'},
-    'KeyJ': {name: 'snare', sound: './assets/sounds/snare.wav'},
-    'KeyK': {name: 'tink', sound: './assets/sounds/tink.wav'},
-    'KeyL': {name: 'tom', sound: './assets/sounds/tom.wav'}
+function removeClass(e) {
+    e.target.classList.remove('playing')
 }
 
+const data = {
+    'KeyA': {key: 'A', name: 'boom', sound: './assets/sounds/boom.wav'},
+    'KeyS': {key: 'S', name: 'clap', sound: './assets/sounds/clap.wav'},
+    'KeyD': {key: 'D', name: 'hi-hat', sound: './assets/sounds/hihat.wav'},
+    'KeyF': {key: 'F', name: 'kick', sound: './assets/sounds/kick.wav'},
+    'KeyG': {key: 'G', name: 'open-hat', sound: './assets/sounds/openhat.wav'},
+    'KeyH': {key: 'H', name: 'ride', sound: './assets/sounds/ride.wav'},
+    'KeyJ': {key: 'J', name: 'snare', sound: './assets/sounds/snare.wav'},
+    'KeyK': {key: 'K', name: 'tink', sound: './assets/sounds/tink.wav'},
+    'KeyL': {key: 'L', name: 'tom', sound: './assets/sounds/tom.wav'}
+}
+
+
 drumSet();
-document.addEventListener('keydown', playDrum)
+document.addEventListener('keydown', playDrum);
+
+const keys = document.querySelectorAll('.drum');
+keys.forEach(key => key.addEventListener('transitionend',  removeClass));
+
